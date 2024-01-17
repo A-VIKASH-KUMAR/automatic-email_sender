2a965144e551913ea7a445795bfb3ce433a4bde8
A-VIKASH-KUMAR <vickyanga@gmail.com>
2024-01-17 22:54:07 +0530

    feat: :sparkles: add auth route file

---
+++ src/routes/auth.route.ts
@@ -0,0 +1,9 @@
+import express from "express"
+import { getGoogleConsentLink, getGmailCredentials } from "../controller/auth.controller";
+
+const router = express.Router();
+
+router.get("/google", getGoogleConsentLink);
+router.get("/google/callback", getGmailCredentials)
+
+export default router;
