125de29a69bfb2d1a2a028ff25340e2e31a81d6a
A-VIKASH-KUMAR <vickyanga@gmail.com>
2024-01-17 21:51:47 +0530

    feat: :sparkles: setup index.ts file

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import authRoute from "./routes/auth.route";
+import emailRoute from "./routes/email.route";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+dotenv.config();
+
+const app = express();
+
+const port = 3001;
+app.use(bodyParser.json());
+
+app.get("/", function (req: any, res: any) {
+  res.send("Hello World");
+  
+});
+
+app.use(express.json());
+
+
+app.listen(port, () => {
+    console.log("server is running on port http://localhost:" + port);
+  });
+
+
+app.use("/auth", authRoute);
+app.use("/send", emailRoute)
