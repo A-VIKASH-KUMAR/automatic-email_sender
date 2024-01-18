5036e3c61dbc90c56d6229585e49b15b85241d97
A-VIKASH-KUMAR <vickyanga@gmail.com>
2024-01-18 16:46:34 +0530

    fix: :bug: removed console.logs

--- src/controller/email.controller.ts
+++ src/controller/email.controller.ts
@@ -29,7 +29,6 @@ const oauth2Client = new google.auth.OAuth2(
 export const sendMessages = async (req: any, res: any) => {
   try {
     const tokens: any = await loadSavedCredentialsIfExist(tokenPath);
-    console.log("tokens", tokens);
     const credentials = JSON.parse(tokens);
     oauth2Client.setCredentials({
       access_token: credentials.access_token,
@@ -39,7 +38,6 @@ export const sendMessages = async (req: any, res: any) => {
     });
 
     const unrepliedMessages: any = await getUnrepliedMessages(oauth2Client);
-    console.log("message ids", unrepliedMessages);
     const label = await createLabel(oauth2Client);
     for (let index = 0; index < unrepliedMessages.length; index++) {
       const threadId = unrepliedMessages[index].threadId;
